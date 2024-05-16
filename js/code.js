7a83849077797f389320bcb4db66fff719529e85
Biel Conde <biel.conde@terramate.io>
2024-05-16 10:26:37 +0200

    feat: add basic game logic

---
+++ js/code.js
@@ -0,0 +1,39 @@
+
+
+/**
+ * 1 - Rock
+ * 2 - Paper
+ * 3 - Scissors
+ */
+
+// define the variables
+const options = {
+	1: "Rock",
+	2: "Paper",
+	3: "Scissors"
+}
+
+let playerSelection = 0
+let computerSelection = 0
+
+// get the player selection
+playerSelection = prompt("Enter your choice: 1 - Rock, 2 - Paper or 3 - Scissors")
+playerSelection = parseInt(playerSelection)
+alert("You chose: " + options[playerSelection])
+
+// get the computer selection
+computerSelection = Math.floor(Math.random() * 3) + 1
+alert("Computer chose: " + options[computerSelection])
+
+// determine the winner
+if (playerSelection === computerSelection) {
+		alert("It's a tie!")
+} else if (playerSelection === 1 && computerSelection === 3) {
+	alert("You win!")
+} else if(playerSelection === 2 && computerSelection === 1) {
+	alert("You win!")
+} else if(playerSelection === 3 && computerSelection === 2) {
+	alert("You win!")
+} else {
+	alert("You lose!")
+}
