56235eacb4981f071461c5e9116ec50f1bd8f94f
Biel Conde <biel.conde@terramate.io>
2024-05-16 10:31:58 +0200

    feat: add loop for best of three

--- js/code.js
+++ js/code.js
@@ -14,26 +14,34 @@ const options = {
 }
 
 let playerSelection = 0
+let playerWins = 0
 let computerSelection = 0
+let computerWins = 0
 
-// get the player selection
-playerSelection = prompt("Enter your choice: 1 - Rock, 2 - Paper or 3 - Scissors")
-playerSelection = parseInt(playerSelection)
-alert("You chose: " + options[playerSelection])
+while (playerWins < 3 && computerWins < 3) {
+	// get the player selection
+	playerSelection = prompt("Enter your choice: 1 - Rock, 2 - Paper or 3 - Scissors")
+	playerSelection = parseInt(playerSelection)
+	alert("You chose: " + options[playerSelection])
 
-// get the computer selection
-computerSelection = Math.floor(Math.random() * 3) + 1
-alert("Computer chose: " + options[computerSelection])
+	// get the computer selection
+	computerSelection = Math.floor(Math.random() * 3) + 1
+	alert("Computer chose: " + options[computerSelection])
 
-// determine the winner
-if (playerSelection === computerSelection) {
+	// determine the winner
+	if (playerSelection === computerSelection) {
 		alert("It's a tie!")
-} else if (playerSelection === 1 && computerSelection === 3) {
-	alert("You win!")
-} else if(playerSelection === 2 && computerSelection === 1) {
-	alert("You win!")
-} else if(playerSelection === 3 && computerSelection === 2) {
-	alert("You win!")
-} else {
-	alert("You lose!")
+	} else if (playerSelection === 1 && computerSelection === 3) {
+		alert("You win!")
+		playerWins++
+	} else if(playerSelection === 2 && computerSelection === 1) {
+		alert("You win!")
+		playerWins++
+	} else if(playerSelection === 3 && computerSelection === 2) {
+		alert("You win!")
+		playerWins++
+	} else {
+		alert("You lose!")
+		computerWins++
+	}
 }
