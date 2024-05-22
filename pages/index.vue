0820217bf7f3340518f895916c4ac92f7eccbf95
Biel Conde <biel.conde@terramate.io>
2024-05-22 10:33:17 +0200

    fix: improve CLS core web vitals

--- pages/index.vue
+++ pages/index.vue
@@ -37,7 +37,7 @@
 							</div>
 							
 							<div class="bg-white rounded-lg shadow-md p-6">
-									<img src="https://via.placeholder.com/300" alt="Product Image" class="mb-4 shadow-lg">
+									<img src="https://via.placeholder.com/300" alt="Product Image" class="mb-4 shadow-lg" width="300" height="300">
 									<h3 class="text-lg font-bold text-gray-800 mb-2">Product Name</h3>
 									<p class="text-gray-600">$XX.XX</p>
 									<a href="#" class="block mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full text-center">Add to Cart</a>
