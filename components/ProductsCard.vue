6a1a982b15143b6f02e4145d51b0ca5d01d486ba
Biel Conde <biel.conde@terramate.io>
2024-05-29 10:37:30 +0200

    feat implement products list and products detail

--- components/ProductsCard.vue
+++ components/ProductsCard.vue
@@ -1,8 +1,25 @@
+<script setup>
+const props = defineProps({
+	product: Object
+})
+
+// const { name, price, brand } = props.product
+</script>
+
+
 <template>
 	<div class="bg-white rounded-lg shadow-md p-6">
 		<img src="https://via.placeholder.com/300" alt="Product Image" class="mb-4">
-		<h3 class="text-lg font-bold text-gray-800 mb-2">Product Name</h3>
-		<p class="text-gray-600">$XX.XX</p>
+		<a 
+			:href="`/product/${product.id}`" 
+			class="text-lg font-bold text-gray-800 mb-2"
+		>
+			{{ product.name }}
+		</a>
+		<h2 class="text-sm font-bold text-gray-600 mb-2">
+			{{ product.brand }}
+		</h2>
+		<p class="text-gray-600">${{ product.price }}</p>
 		<a href="#" class="block mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full text-center">Add to Cart</a>
 	</div>
 </template>
